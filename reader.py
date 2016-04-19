4da9f2c1a1d78b657a0868328cef04c83904e430
Maim <jhilum@hotmail.com>
2016-04-20 02:53:15 +0300

    initial commit

---
+++ reader.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+str = "python"
+print ("hello "+str)
+ 
+
+# Open a file
+fo = open("page471.txt", "r+")
+page = fo.read();
+
+rtf_segment = page.split(") # but keep just the odd numbered
+final_rtf_segments = ONLY_THE_ODD_rtf_segment.split('')[0]
+
+#print ("Read String is : ", str)
+
+# Close opend file
+fo.close()
